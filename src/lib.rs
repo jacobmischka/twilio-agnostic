3e28d4aa516e1a4ae196139ed252e1edf3c7272e
Jacob Mischka <jacob@mischka.me>
2021-07-08 15:27:34 -0500

    Use isahc instead of hyper, remove webhook support

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,24 @@
 mod call;
 mod message;
 pub mod twiml;
-mod webhook;
 
 pub use call::{Call, OutboundCall};
-use headers::authorization::{Authorization, Basic};
-use headers::{ContentType, HeaderMapExt};
-use hyper::client::connect::HttpConnector;
-use hyper::{Body, Method, StatusCode};
-use hyper_tls::HttpsConnector;
+use http::{
+    header::{HeaderValue, CONTENT_TYPE},
+    Method, StatusCode,
+};
+use isahc::{
+    auth::{Authentication, Credentials},
+    config::Configurable,
+    AsyncBody, AsyncReadResponseExt,
+};
 pub use message::{Message, OutboundMessage};
-use std::collections::BTreeMap;
-use std::error::Error;
-use std::fmt::{self, Display, Formatter};
+
+use std::{
+    collections::BTreeMap,
+    error::Error,
+    fmt::{self, Display, Formatter},
+};
 
 pub const GET: Method = Method::GET;
 pub const POST: Method = Method::POST;
@@ -21,8 +27,6 @@ pub const PUT: Method = Method::PUT;
 pub struct Client {
     account_id: String,
     auth_token: String,
-    auth_header: Authorization<Basic>,
-    http_client: hyper::Client<HttpsConnector<HttpConnector>>,
 }
 
 fn url_encode(params: &[(&str, &str)]) -> String {
@@ -41,7 +45,8 @@ fn url_encode(params: &[(&str, &str)]) -> String {
 
 #[derive(Debug)]
 pub enum TwilioError {
-    NetworkError(hyper::Error),
+    NetworkError(http::Error),
+    TransmissionError(isahc::error::Error),
     HTTPError(StatusCode),
     ParsingError,
     AuthError,
@@ -52,6 +57,7 @@ impl Display for TwilioError {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         match *self {
             TwilioError::NetworkError(ref e) => e.fmt(f),
+            TwilioError::TransmissionError(ref e) => e.fmt(f),
             TwilioError::HTTPError(ref s) => write!(f, "Invalid HTTP status code: {}", s),
             TwilioError::ParsingError => f.write_str("Parsing error"),
             TwilioError::AuthError => f.write_str("Missing `X-Twilio-Signature` header in request"),
@@ -78,78 +84,47 @@ impl Client {
         Client {
             account_id: account_id.to_string(),
             auth_token: auth_token.to_string(),
-            auth_header: Authorization::basic(account_id, auth_token),
-            http_client: hyper::Client::builder().build(HttpsConnector::new()),
         }
     }
 
     async fn send_request<T>(
         &self,
-        method: hyper::Method,
+        method: http::Method,
         endpoint: &str,
         params: &[(&str, &str)],
     ) -> Result<T, TwilioError>
     where
-        T: serde::de::DeserializeOwned,
+        T: serde::de::DeserializeOwned + std::marker::Unpin,
     {
         let url = format!(
             "https://api.twilio.com/2010-04-01/Accounts/{}/{}.json",
             self.account_id, endpoint
         );
-        let mut req = hyper::Request::builder()
+        let req = isahc::Request::builder()
             .method(method)
-            .uri(&*url)
-            .body(Body::from(url_encode(params)))
-            .unwrap();
+            .uri(&url)
+            .header(
+                CONTENT_TYPE,
+                HeaderValue::from_static("application/x-www-form-urlencoded"),
+            )
+            .authentication(Authentication::basic())
+            .credentials(Credentials::new(
+                self.account_id.clone(),
+                self.auth_token.clone(),
+            ))
+            .body(AsyncBody::from(url_encode(params)))
+            .map_err(|e| TwilioError::NetworkError(e))?;
 
-        let mime: mime::Mime = "application/x-www-form-urlencoded".parse().unwrap();
-        req.headers_mut().typed_insert(ContentType::from(mime));
-        req.headers_mut().typed_insert(self.auth_header.clone());
-
-        let resp = self
-            .http_client
-            .request(req)
+        let mut resp = isahc::send_async(req)
             .await
-            .map_err(TwilioError::NetworkError)?;
+            .map_err(TwilioError::TransmissionError)?;
 
         match resp.status() {
-            StatusCode::CREATED | StatusCode::OK => {}
-            other => return Err(TwilioError::HTTPError(other)),
-        };
-
-        let decoded: T = hyper::body::to_bytes(resp.into_body())
-            .await
-            .map_err(TwilioError::NetworkError)
-            .and_then(|bytes| {
-                serde_json::from_slice(&bytes).map_err(|_| TwilioError::ParsingError)
-            })?;
-
-        Ok(decoded)
-    }
-
-    pub async fn respond_to_webhook<T: FromMap, F>(
-        &self,
-        req: hyper::Request<Body>,
-        mut logic: F,
-    ) -> hyper::Response<Body>
-    where
-        F: FnMut(T) -> twiml::Twiml,
-    {
-        let o: T = match self.parse_request::<T>(req).await {
-            Ok(obj) => *obj,
-            Err(_) => {
-                let mut res = hyper::Response::new(Body::from("Error.".as_bytes()));
-                *res.status_mut() = StatusCode::BAD_REQUEST;
-                return res;
+            StatusCode::CREATED | StatusCode::OK => {
+                let value: T = resp.json().await.map_err(|_| TwilioError::ParsingError)?;
+                Ok(value)
             }
-        };
-
-        let t = logic(o);
-        let body = t.as_twiml();
-        let len = body.len() as u64;
-        let mut res = hyper::Response::new(Body::from(body));
-        res.headers_mut().typed_insert(headers::ContentType::xml());
-        res.headers_mut().typed_insert(headers::ContentLength(len));
-        res
+            other => return Err(TwilioError::HTTPError(other)),
+        }
     }
 }
